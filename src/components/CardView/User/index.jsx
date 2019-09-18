7284ad850833c280d259caf1ac40bd665696c3c8
Gilmar Soares <gilmar.santos@gmmti.com.br>
2019-09-18 16:43:45 -0300

    Inclui estilizaçao para responsividade

--- src/components/CardView/User/index.jsx
+++ src/components/CardView/User/index.jsx
@@ -12,139 +12,141 @@ import MessageNoItems from '../../../components/MessageNoItems';
 import './style.css';
 
 export default function UserCard(props) {
-    const showButtons = props.showButtons === undefined || props.showButtons;
-    const showMenuInfo = props.showMenuInfo === undefined || props.showMenuInfo;
-    const showIconUp = props.showIconUp || false;
-    const showIconDown = props.showIconDown || false;
-    const [showMenu, setShowMenu] = useState(false);
-    const [selectedItem, setSelectedItem] = useState(null);
-    const [itemsCount, setItemsCount] = useState(1);
-    const { languages, jobs } = props.extraInfo;
+	const showButtons = props.showButtons === undefined || props.showButtons;
+	const showMenuInfo = props.showMenuInfo === undefined || props.showMenuInfo;
+	const showIconUp = props.showIconUp || false;
+	const showIconDown = props.showIconDown || false;
+	const [showMenu, setShowMenu] = useState(false);
+	const [selectedItem, setSelectedItem] = useState(null);
+	const [itemsCount, setItemsCount] = useState(1);
+	const { languages, jobs } = props.extraInfo;
 
-    return (
-        <div className='card-user'>
-            <img src={props.avatar} alt={props.name} className='avatar' />
-            {showIconUp && (
-                <span className='icon-up-container'>
-                    <ThumbUp />
-                </span>
-            )}
-            {showIconDown && (
-                <span className='icon-down-container'>
-                    <ThumbDown />
-                </span>
-            )}
-            {showMenuInfo && (
-                <span className='more-information'>
-                    <MoreIcon onClick={() => setShowMenu(true)} />
-                </span>
-            )}
-            {showMenu && (
-                <Fragment>
-                    <div className='menu'>
-                        <h4>Mais informações</h4>
-                        <ul>
-                            <li
-                                onClick={() => {
-                                    setSelectedItem('language');
-                                    setItemsCount(
-                                        languages && languages.length
-                                    );
-                                }}
-                            >
-                                Tecnologias
-                            </li>
-                            <li
-                                onClick={() => {
-                                    setSelectedItem('jobs');
-                                    setItemsCount(jobs && jobs.length);
-                                }}
-                            >
-                                Jobs
-                            </li>
-                        </ul>
-                        <button onClick={e => setShowMenu(false)}>
-                            Fechar
-                        </button>
-                    </div>
-                    {selectedItem && (
-                        <div className='additional-info-content'>
-                            <h4>
-                                {selectedItem === 'jobs'
-                                    ? 'Jobs'
-                                    : 'Tecnologias'}
-                            </h4>
-                            <div className='info-conteiner'>
-                                <ul className={selectedItem}>
-                                    {selectedItem === 'language' &&
-                                        languages &&
-                                        languages.map(item => (
-                                            <li key={item}>{item}</li>
-                                        ))}
-                                    {selectedItem === 'jobs' &&
-                                        jobs &&
-                                        jobs.map(job => (
-                                            <li key={job.name}>
-                                                <a
-                                                    href={job.url}
-                                                    target='_blank'
-                                                    rel='noopener noreferrer'
-                                                >
-                                                    {job.name}
-                                                </a>
-                                            </li>
-                                        ))}
-                                </ul>
-                                <MessageNoItems
-                                    items={itemsCount}
-                                    message='Não há itens disponíveis para esta seção.'
-                                />
-                            </div>
-                            <button
-                                onClick={e => {
-                                    setSelectedItem(null);
-                                    setShowMenu(false);
-                                }}
-                            >
-                                Fechar
-                            </button>
-                        </div>
-                    )}
-                </Fragment>
-            )}
-            <footer>
-                <strong>{props.name}</strong>
-                <a
-                    target='_blank'
-                    rel='noopener noreferrer'
-                    className='github-user'
-                    href={`https://github.com/${props.username}`}
-                >
-                    <UserIcon fontSize='small' /> <i>{props.username}</i>
-                </a>
+	return (
+		<div className='card-user'>
+			<header>
+				<img src={props.avatar} alt={props.name} className='avatar' />
+				{showIconUp && (
+					<span className='icon-up-container'>
+						<ThumbUp />
+					</span>
+				)}
+				{showIconDown && (
+					<span className='icon-down-container'>
+						<ThumbDown />
+					</span>
+				)}
+				{showMenuInfo && (
+					<span className='more-information'>
+						<MoreIcon onClick={() => setShowMenu(true)} />
+					</span>
+				)}
+				{showMenu && (
+					<Fragment>
+						<div className='menu'>
+							<h4>Mais informações</h4>
+							<ul>
+								<li
+									onClick={() => {
+										setSelectedItem('language');
+										setItemsCount(
+											languages && languages.length
+										);
+									}}
+								>
+									Tecnologias
+								</li>
+								<li
+									onClick={() => {
+										setSelectedItem('jobs');
+										setItemsCount(jobs && jobs.length);
+									}}
+								>
+									Jobs
+								</li>
+							</ul>
+							<button onClick={e => setShowMenu(false)}>
+								Fechar
+							</button>
+						</div>
+						{selectedItem && (
+							<div className='additional-info-content'>
+								<h4>
+									{selectedItem === 'jobs'
+										? 'Jobs'
+										: 'Tecnologias'}
+								</h4>
+								<div className='info-conteiner'>
+									<ul className={selectedItem}>
+										{selectedItem === 'language' &&
+											languages &&
+											languages.map(item => (
+												<li key={item}>{item}</li>
+											))}
+										{selectedItem === 'jobs' &&
+											jobs &&
+											jobs.map(job => (
+												<li key={job.name}>
+													<a
+														href={job.url}
+														target='_blank'
+														rel='noopener noreferrer'
+													>
+														{job.name}
+													</a>
+												</li>
+											))}
+									</ul>
+									<MessageNoItems
+										items={itemsCount}
+										message='Não há itens disponíveis para esta seção.'
+									/>
+								</div>
+								<button
+									onClick={e => {
+										setSelectedItem(null);
+										setShowMenu(false);
+									}}
+								>
+									Fechar
+								</button>
+							</div>
+						)}
+					</Fragment>
+				)}
+			</header>
+			<footer>
+				<strong>{props.name}</strong>
+				<a
+					target='_blank'
+					rel='noopener noreferrer'
+					className='github-user'
+					href={`https://github.com/${props.username}`}
+				>
+					<UserIcon fontSize='small' /> <i>{props.username}</i>
+				</a>
 
-                <span className={props.location ? 'location' : 'hide'}>
-                    <LocalIcon fontSize='small' />
-                    {props.location}
-                </span>
-                <p>
-                    {props.description || (languages && languages.join(', '))}
-                </p>
-            </footer>
-            {showButtons && (
-                <div className='buttons'>
-                    <Button
-                        alt='Like'
-                        img={like}
-                        onClick={() => props.likeCallback(props.id)}
-                    />
-                    <Button
-                        alt='Unlike'
-                        img={unlike}
-                        onClick={() => props.unlikeCallback(props.id)}
-                    />
-                </div>
-            )}
-        </div>
-    );
+				<span className={props.location ? 'location' : 'hide'}>
+					<LocalIcon fontSize='small' />
+					{props.location}
+				</span>
+				<p>
+					{props.description || (languages && languages.join(', '))}
+				</p>
+			</footer>
+			{showButtons && (
+				<div className='buttons'>
+					<Button
+						alt='Like'
+						img={like}
+						onClick={() => props.likeCallback(props.id)}
+					/>
+					<Button
+						alt='Unlike'
+						img={unlike}
+						onClick={() => props.unlikeCallback(props.id)}
+					/>
+				</div>
+			)}
+		</div>
+	);
 }
